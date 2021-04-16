f6c5d7abc4ca16f43ddd5b750507b75b689d02d2
James Tiu <jamestiulawyer@gmail.com>
2021-04-16 12:05:05 -0600

    dictionary works

--- Dictionary.cs
+++ Dictionary.cs
@@ -14,23 +14,68 @@ namespace CourtHearing
         public string RegistrationID { get; set; }
         public string NextCourtDate { get; set; }
 
-
-
-
         public CourthouseParticipant CreateDictionaryParticipants()
         {
-            
-            CourthouseParticipant ctHouseParticipant1 = new CourthouseParticipant();
-            FirstName = "Jessica";
-            LastName = "Jones";
-            EmailAddress = "Jess@JS.com";
-            RegistrationID = "345";
-            NextCourtDate = "Apr25";
-            Console.WriteLine($"Dictionary Class, method = 'createDictionaryParticipants' First/LastName = {FirstName}{LastName}");
-            Console.ReadLine();
-            return ctHouseParticipant1;
-        }
 
+            CourthouseParticipant ctHouseParticipant1 = new CourthouseParticipant()
+            {
+                FirstName = "Jessica",
+                LastName = "Jones",
+                EmailAddress = "Jess@JS.com",
+                RegistrationID = "345",
+                NextCourtDate = "Apr25",
+
+            };
+
+            CourthouseParticipant ctHouseParticipant2 = new CourthouseParticipant()
+            {
+                FirstName = "Joe",
+                LastName = "Smith",
+                EmailAddress = "Joe@JS.com",
+                RegistrationID = "123",
+                NextCourtDate = "Apr24",
+
+            };
+
+            CourthouseParticipant ctHouseParticipant3 = new CourthouseParticipant()
+            {
+                FirstName = "Jeff",
+                LastName = "Jones",
+                EmailAddress = "Jeff@JS.com",
+                RegistrationID = "234",
+                NextCourtDate = "Apr22",
+
+            };
+
+            Dictionary<string, CourthouseParticipant> dictionaryCtHouseParticipants = new Dictionary<string, CourthouseParticipant>();
+            dictionaryCtHouseParticipants.Add(ctHouseParticipant1.RegistrationID, ctHouseParticipant1);
+            dictionaryCtHouseParticipants.Add(ctHouseParticipant2.RegistrationID, ctHouseParticipant2);
+            dictionaryCtHouseParticipants.Add(ctHouseParticipant3.RegistrationID, ctHouseParticipant3);
+
+            CourthouseParticipant courthouseParticipant345 = dictionaryCtHouseParticipants["345"];
+
+            CourthouseParticipant courthouseParticipant123 = dictionaryCtHouseParticipants["123"];
+
+            CourthouseParticipant courthouseParticipant234 = dictionaryCtHouseParticipants["234"];
+          
+            Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant345.RegistrationID, courthouseParticipant345.FirstName, courthouseParticipant345.LastName, courthouseParticipant345.NextCourtDate);
+          
+            Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant123.RegistrationID, courthouseParticipant123.FirstName, courthouseParticipant123.LastName, courthouseParticipant123.NextCourtDate);
+       
+            Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant234.RegistrationID, courthouseParticipant234.FirstName, courthouseParticipant234.LastName, courthouseParticipant234.NextCourtDate);
+            Console.ReadLine();
+
+            foreach (KeyValuePair<string, CourthouseParticipant> participantKeyValuePair in dictionaryCtHouseParticipants)
+            {
+                Console.WriteLine("@hereID = {0}", participantKeyValuePair.Key);
+                CourthouseParticipant ctHousePart = participantKeyValuePair.Value;
+                Console.WriteLine("ID = {0}, FirstName = {1}, LastName = {2}, NextCourtDate = {3}", ctHousePart.RegistrationID, ctHousePart.FirstName, ctHousePart.LastName, ctHousePart.NextCourtDate);
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                Console.ReadLine();
+            }
+            return courthouseParticipant123;
+        }
+       
     }
 
 }   
